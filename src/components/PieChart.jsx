1092b48940bfd4532048c78284fc804a240d73b2
Prem <premkhodke.99@gmail.com>
2024-06-19 12:39:01 +0530

    pie chart

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,45 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import { getPieChart } from '../services/api';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 const PieChart = ({ month }) => {
-  // const [pieChartData, setPieChartData] = useState([]);
+  const [pieChartData, setPieChartData] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const { data } = await getPieChart({ month });
-  //     setPieChartData(data);
-  //   };
-  //   fetchData();
-  // }, [month]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await getPieChart({ month });
+      setPieChartData(data);
+    };
+    fetchData();
+  }, [month]);
 
-  // const data = {
-  //   labels: pieChartData.map(item => item._id),
-  //   datasets: [
-  //     {
-  //       label: '# of Items',
-  //       data: pieChartData.map(item => item.count),
-  //       backgroundColor: [
-  //         '#FF6384',
-  //         '#36A2EB',
-  //         '#FFCE56',
-  //         '#FF6384',
-  //         '#36A2EB',
-  //         '#FFCE56',
-  //       ],
-  //       hoverBackgroundColor: [
-  //         '#FF6384',
-  //         '#36A2EB',
-  //         '#FFCE56',
-  //         '#FF6384',
-  //         '#36A2EB',
-  //         '#FFCE56',
-  //       ],
-  //     },
-  //   ],
-  // };
+  const data = {
+    labels: pieChartData.map(item => item._id),
+    datasets: [
+      {
+        label: '# of Items',
+        data: pieChartData.map(item => item.count),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-  // return <Pie data={data} />;
+  return <div className='w-1/3 h-1/3'>
+    <Pie data={data} />
+  </div>;
 }
 
 export default PieChart
