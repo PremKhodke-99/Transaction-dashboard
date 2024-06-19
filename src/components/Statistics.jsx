aab430db8c488efbb0769aa50cdcecdaadb50592
Prem <premkhodke.99@gmail.com>
2024-06-19 12:09:29 +0530

    stats

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -14,8 +14,8 @@ const Statistics = ({ month }) => {
 
 
   return (
-    <div>
-      <h3>Statistics for {month}</h3>
+    <div className='h-[150px] w-60 bg-yellow-200 rounded-xl p-3 border-none leading-8'>
+      <h3 className='text-lg'>Statistics for: <strong>{month}</strong></h3>
       <p>Total Sale Amount: {statistics?.totalSaleAmount}</p>
       <p>Total Sold Items: {statistics?.totalSoldItems}</p>
       <p>Total Not Sold Items: {statistics?.totalNotSoldItems}</p>
