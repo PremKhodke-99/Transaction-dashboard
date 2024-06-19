5634f639a4700dca40e5edd955c2dec681b408e6
Prem <premkhodke.99@gmail.com>
2024-06-19 11:56:00 +0530

    cors config

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const dbConnect = require('./db/dbConfig');
 const transactionRouter = require('./routes/transactions.routes');
 
 const app = express();
-
 dbConnect();
 
+app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.use('/api', transactionRouter);
 const PORT = process.env.PORT || 3001;
